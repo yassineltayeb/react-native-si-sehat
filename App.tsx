63ea560ac9705c67243662225b337c4e117bcb7d
Yassin Eltayeb <yassineltayeb@live.com>
2024-07-14 16:34:49 +0400

    fixed tailwind css issue

--- App.tsx
+++ App.tsx
@@ -9,24 +9,26 @@ import { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { useDeviceContext, useAppColorScheme } from "twrnc";
 import tw from "./lib/tailwind";
+import { View } from "react-native";
 
 export default function App() {
   const { colorScheme, toggleColorScheme, setColorScheme } = useColorScheme();
 
   useEffect(() => {
-    setColorScheme("dark"); 
+    setColorScheme("dark");
   }, []);
-  
+
   useDeviceContext(tw, {
     observeDeviceColorSchemeChanges: false,
     initialColorScheme: "dark",
-  },);
+  });
+
 
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaView className="flex-1 bg-white dark:bg-dark-900">
-          <StatusBar style={colorScheme == "dark" ? "light" : "dark"} />
+          {/* <StatusBar style={colorScheme == "dark" ? "light" : "dark"} /> */}
           <StackNavigation />
         </SafeAreaView>
       </NavigationContainer>
