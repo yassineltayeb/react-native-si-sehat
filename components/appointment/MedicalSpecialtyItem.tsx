6d24194fe97af60815c9a33dd2590ff6277c9924
Yassin Eltayeb <yassineltayeb@live.com>
2024-07-24 22:07:52 +0400

    finished medical specialties

--- components/appointment/MedicalSpecialtyItem.tsx
+++ components/appointment/MedicalSpecialtyItem.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import { MedicalSpecialty } from "../../models/appointment/medical-specialty.model";
 import AppointmentTitle from "./AppointmentTitle";
 import SubTitle from "../common/labels/SubTitle";
+import { Ionicons } from "@expo/vector-icons"; // Adjust the import based on the icon library you're using
+import { useColorScheme } from "nativewind";
+import { ColorScheme } from "../../enums/ColorScheme.enum";
+import ButtonLabel from "../common/buttons/ButtonLabel";
 
 interface MedicalSpecialtyItemProps {
   medicalSpecialty: MedicalSpecialty;
@@ -11,20 +15,29 @@ interface MedicalSpecialtyItemProps {
 const MedicalSpecialtyItem: React.FC<MedicalSpecialtyItemProps> = ({
   medicalSpecialty,
 }) => {
+  const { colorScheme } = useColorScheme();
+
   return (
     <View className="my-5">
-      <View className="flex flex-row">
-        {/* Icon */}
-        <View className="bg-primary-50 dark:bg-primary-900 border border-primary-100 dark:border-primary-800 rounded-full h-[52] w-[52] justify-center items-center">
-          <Text className="font-manrope-bold font-bold text-3xl">
-            {medicalSpecialty.image}
-          </Text>
-        </View>
-        <View className="ml-4">
-          <AppointmentTitle title={medicalSpecialty.title} />
-          <View className="mt-1">
-            <SubTitle title={medicalSpecialty.description} />
+      <View className="flex flex-row justify-between items-center">
+        <View className="flex flex-row">
+          <View className="flex flex-row">
+            {/* Icon */}
+            <View className="bg-primary-50 dark:bg-primary-900 border border-primary-100 dark:border-primary-800 rounded-full h-[52] w-[52] justify-center items-center">
+              <Text className="font-manrope-bold font-bold text-3xl">
+                {medicalSpecialty.image}
+              </Text>
+            </View>
+            <View className="ml-4">
+              <AppointmentTitle title={medicalSpecialty.title} />
+              <View className="mt-1">
+                <SubTitle title={medicalSpecialty.description} />
+              </View>
+            </View>
           </View>
+        </View>
+        <View>
+          <Ionicons name="chevron-forward-outline" size={24} color={colorScheme === ColorScheme.Light ?"#254EDB" :"#4F73DF"} />
         </View>
       </View>
     </View>
